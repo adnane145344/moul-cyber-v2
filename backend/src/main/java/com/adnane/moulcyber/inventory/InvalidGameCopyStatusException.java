0763cbda615613e30160077bce8ff4f4eabf6d0f
Adnane Lardi <adnane.lardi.1@ulaval.ca>
2026-06-22 09:47:34 -0400

    feat: implement game copy domain rules

---
+++ backend/src/main/java/com/adnane/moulcyber/inventory/InvalidGameCopyStatusException.java
@@ -0,0 +1,8 @@
+package com.adnane.moulcyber.inventory;
+
+public class InvalidGameCopyStatusException extends IllegalStateException {
+
+    public InvalidGameCopyStatusException(String message) {
+        super(message);
+    }
+}
